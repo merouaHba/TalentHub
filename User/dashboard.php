fa4c9f6c1047b017c012306bfaceb3c90764b244
wass <wassimbs088@gmail.com>
2023-12-21 00:52:11 +0100

    add view/

--- User/dashboard.php
+++ User/dashboard.php
@@ -2,14 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Successeful <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
     <title>Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -42,7 +34,7 @@
                     </div>
                     <?php
                     // Include config file
-                    include "./config.php";
+                    include "config.php";
                     
                     // Attempt select query execution
                     $sql = "SELECT * FROM Offer";
