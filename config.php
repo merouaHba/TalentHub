d7bd665875c065ba1a3ee2102b0a5c4074acd64a
wass <wassimbs088@gmail.com>
2023-12-21 00:42:34 +0100

    add dashboard.php

--- config.php
+++ config.php
@@ -9,13 +9,18 @@ try {
 	$pdo = new PDO($dsn, $username, $password);
 
 	if ($pdo) {
-        // creation of DataBase talent
-        $pdo->exec("");
         $DDL = [
-            "CREATE DATABASE IF NOT EXISTS talent;",
+        "CREATE DATABASE IF NOT EXISTS talent;",
         "CREATE TABLE IF NOT EXISTS User(id INT AUTO_INCREMENT PRIMARY KEY,username VARCHAR(10) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(60) NOT NULL,name VARCHAR(50) NOT NULL, resume BLOB);",
-        // Offer  (id, name, Company(FK), )
-        ];
+        "CREATE TABLE IF NOT EXISTS Company(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50) NOT NULL, location VARCHAR(20) NOT NULL);",
+        "CREATE TABLE IF NOT EXISTS Offer(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50) NOT NULL, location VARCHAR(20) NOT NULL,available BOOLEAN NOT NULL ,FOREIGN KEY (CompanyID) REFERENCES Company(id));",
+        "CREATE TABLE IF NOT EXISTS Application(id INT PRIMARY KEY AUTO_INCREMENT,FOREIGN KEY (StudentID) REFERENCES Student(id) ,FOREIGN KEY (OfferID) REFERENCES Offer(id) ,FOREIGN KEY (CompanyID) REFERENCES Company(id));"
+    ];
+    foreach($DDL as $command)
+    {
+        $pdo->exec($command);
+    }
+      
 	}
 } catch (PDOException $e) {
 	echo $e->getMessage();
